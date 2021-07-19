3dc5acecab53c512fcedf964c07c3c2dcdb04190
Ben <3>
2021-07-18 23:20:33 -0700

    Added HomeScreen

--- FoodBankBase/Main Views/HomeScreenButtons/Map/FoodBankMapView.swift
+++ FoodBankBase/Main Views/HomeScreenButtons/Map/FoodBankMapView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 
 struct FoodBankMapView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 47.66558123405208, longitude: -122.38048848981833), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
 
     var body: some View {
         Map(coordinateRegion: $region)
