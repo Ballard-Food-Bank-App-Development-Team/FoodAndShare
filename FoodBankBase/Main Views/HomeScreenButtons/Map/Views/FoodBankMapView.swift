890a9345f722ac7b837d888a6fff3adf468f0a78
cactusbranch01 <benradroberts@gmail.com>
2021-10-13 11:40:48 -0700

    final commit?

--- FoodBankBase/Main Views/HomeScreenButtons/Map/Views/FoodBankMapView.swift
+++ FoodBankBase/Main Views/HomeScreenButtons/Map/Views/FoodBankMapView.swift
@@ -27,15 +27,17 @@ struct FoodBankMapView: View {
             longitude: -122.38048848981833
         ),
         span: MKCoordinateSpan(
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.09
+            latitudeDelta: 0.08,
+            longitudeDelta: 0.08
         )
     )
     
     let places = [
         place(name: "Ballard Food Bank", latitude: 47.66558123405208, longitude: -122.38048848981833, image: "gearshape.fill", color: "customOrange"),
         place(name: "Sound Health", latitude: 47.662342, longitude: -122.341719, image: "cross.circle", color: "customRed"),
-        place(name: "Ballard Library", latitude: 47.66981, longitude: -122.384271, image: "book.circle", color: "customPurple")
+        place(name: "Ballard Library", latitude: 47.66981, longitude: -122.384271, image: "book.circle", color: "customPurple"),
+        place(name: "Wallingford Foodbank", latitude: 47.692046, longitude: -122.355477, image: "house", color: "wallingfordPurple")
+        // place(name: "Queen Anne Foodbank", latitude: 47.622258334842456, longitude: -122.35386959247157, image: "heart.circle", color: "wallingfordPurple")
     ]
     
     @Environment(\.presentationMode) var presentation
@@ -54,10 +56,9 @@ struct FoodBankMapView: View {
                     }
                 }
                 .frame(width: 400, height: 450)
-                .padding()
+                .padding(.top)
                 Divider()
                 HStack {
-                    Spacer()
                     Spacer()
                     Spacer()
                     Image(systemName: "gearshape.fill")
@@ -80,11 +81,9 @@ struct FoodBankMapView: View {
                         .foregroundColor(Color("textGrey"))
                         .font(.custom("HelveticaNeue-Medium", size: 14))
                     Spacer()
-                    Spacer()
                 }
                 Divider()
                 HStack {
-                    Spacer()
                     Spacer()
                     Spacer()
                     Image(systemName: "cross.circle")
@@ -107,11 +106,9 @@ struct FoodBankMapView: View {
                         .foregroundColor(Color("textGrey"))
                         .font(.custom("HelveticaNeue-Medium", size: 14))
                     Spacer()
-                    Spacer()
                 }
                 Divider()
                 HStack {
-                    Spacer()
                     Spacer()
                     Spacer()
                     Image(systemName: "book.circle")
@@ -134,9 +131,58 @@ struct FoodBankMapView: View {
                         .foregroundColor(Color("textGrey"))
                         .font(.custom("HelveticaNeue-Medium", size: 14))
                     Spacer()
+                }
+                Divider()
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Image(systemName: "house")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 35, height: 35)
+                        .clipShape(Circle())
+                        .foregroundColor(Color("wallingfordPurple"))
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Greenwood Food") // 20
+                            .foregroundColor(Color("navyBlue"))
+                            .font(.custom("HelveticaNeue-Medium", size: 20))
+                        Text("Food/family resources")//
+                            .foregroundColor(Color("textGrey"))
+                            .font(.custom("HelveticaNeue-Medium", size: 16))
+                    }
+                    Spacer()
+                    Text("1501 North 45th Street") // 19
+                        .foregroundColor(Color("textGrey"))
+                        .font(.custom("HelveticaNeue-Medium", size: 14))
                     Spacer()
                 }
                 Divider()
+                /*HStack {
+                    Spacer()
+                    Spacer()
+                    Image(systemName: "heart.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 35, height: 35)
+                        .clipShape(Circle())
+                        .foregroundColor(Color("queenAnneGreen"))
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("Queen Anne Food") // 17
+                            .foregroundColor(Color("navyBlue"))
+                            .font(.custom("HelveticaNeue-Medium", size: 20))
+                        Text("Food and resources")// 16
+                            .foregroundColor(Color("textGrey"))
+                            .font(.custom("HelveticaNeue-Medium", size: 16))
+                    }
+                    Spacer()
+                    Text("205 2nd Ave N") // 19
+                        .foregroundColor(Color("textGrey"))
+                        .font(.custom("HelveticaNeue-Medium", size: 14))
+                    Spacer()
+                }
+                 */
             }
         }
         .navigationBarTitleDisplayMode(.inline)
